f06a331791ed012d101a91ebb22d9a640d685c12
DogeyStamp <adeng.sean@gmail.com>
2021-07-21 13:58:13 -0400

    Increase inner gaps

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 40;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 40;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
