aabb8bb615ceb64045378dbb4ba2b88d613d46e8
DogeyStamp <adeng.sean@gmail.com>
2021-07-21 14:45:52 -0400

    Change font to Roboto

--- config.def.h
+++ config.def.h
@@ -8,14 +8,14 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 40;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 40;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 50;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static       int passthrough        = 0;        /* 1 means to ignore most shortcuts */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=12" };
-static const char dmenufont[]       = "JetBrains Mono:size=13";
+static const char *fonts[]          = { "Roboto-Thin:size=14" };
+static const char dmenufont[]       = "Roboto-Thin:size=13";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#eeeeee";
 static const char col_gray3[]       = "#dddddd";
