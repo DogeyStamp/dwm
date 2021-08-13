60a64c55525d3f3d611573818ee60f846068b080
DogeyStamp <dogeystamp@disroot.org>
2021-08-13 17:05:13 -0400

    Increase gaps

--- config.def.h
+++ config.def.h
@@ -6,10 +6,10 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 50;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 50;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 100;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 175;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static       int passthrough        = 0;        /* 1 means to ignore most shortcuts */
 static const int showbar            = 1;        /* 0 means no bar */
